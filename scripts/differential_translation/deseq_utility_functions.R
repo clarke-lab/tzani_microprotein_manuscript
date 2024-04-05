657a5ea6515e0f8198d495763c243a64a1a72436
colin986 <colin.clarke@nibrt.ie>
2024-04-05 15:03:01 +0000

    roll back for lfcShrikage addtion

--- scripts/differential_translation/deseq_utility_functions.R
+++ scripts/differential_translation/deseq_utility_functions.R
@@ -1,165 +1,146 @@
-run_deseq <-
-  function(count_data,
-           sample_table,
-           fold_change_thresh,
-           pval_thresh,
-           base_mean_thresh,
-           average_counts,
-           type,
-           CriGri_PICRH_1_0_annotation,
-           mouse_feature_table) {
+
+run_deseq <-  function(count_data, sample_table, fold_change_thresh,
+pval_thresh,base_mean_thresh,average_counts,type, 
+CriGri_PICRH_1_0_annotation,mouse_feature_table) {
     
+  # filter genes by minimum read criteria
+  if (type == "translation") {
     
-    if (type == "translation") {
-      rna_counts <- count_data %>%
-        dplyr::select(contains("rna"))
-      ribo_counts <- count_data %>%
-        dplyr::select(contains("ribo"))
-      
-      detected.rna <-
-        rownames(rna_counts)[rowMeans(rna_counts) >= average_counts]
-      
-      detected.ribo <-
-        rownames(rna_counts)[rowMeans(ribo_counts) >= average_counts]
-      detected.both <- intersect(detected.rna, detected.ribo)
-      
-      print(length("detected.both"))
-      
-      counts <- count_data[detected.both, ]
-      
-    } else {
-      counts <- count_data %>%
-        dplyr::select(contains(type))
-      
-      detected.genes <-
-        rownames(counts)[rowMeans(counts) >= average_counts]
-      
-      counts <- counts[detected.genes,]
-    }
+    rna_counts <- count_data %>%
+      dplyr::select(contains("rna"))
+    ribo_counts <- count_data %>%
+      dplyr::select(contains("ribo"))
     
-    if (type == "translation") {
-      dds <- DESeqDataSetFromMatrix(
-        countData = counts,
-        colData = sample_table,
-        design = ~ condition + assay + condition:assay
-      )
-      
-      dds$condition <- relevel(dds$condition, ref = "nts")
-      dds$assay <- relevel(dds$assay, ref = "rnaseq")
-    } else {
-      design <- sample_table %>%
-        filter(assay == type)
-      
-      dds <-
-        DESeqDataSetFromMatrix(
-          countData = counts,
-          colData = design,
-          design = ~ condition
-        )
-      
-      dds$condition <- relevel(dds$condition, ref = "nts")
-    }
+    detected.rna <-
+      rownames(rna_counts)[rowMeans(rna_counts) >= average_counts]
     
-    print(head(counts))
-    print(design)
+    detected.ribo <-
+      rownames(rna_counts)[rowMeans(ribo_counts) >= average_counts]
+    detected.both <- intersect(detected.rna, detected.ribo)
     
-    dds <- estimateSizeFactors(dds)
-    dds <- estimateDispersions(dds)
-    dds <- DESeq(dds)
+    print(length("detected.both"))
     
-    # track plotting scaling
-    # bam_coverage_scaling_rnaseq <- 1 / sizeFactors(dds_rnaseq)
-    # write_lines(bam_coverage_scaling_rnaseq, paste(results_dir, "rnaseq_size_factors.txt", sep = ""))
+    counts <- count_data[detected.both, ]
     
-    if (type == "translation") {
-      res = results(dds, name = "conditionts.assayriboseq")
-    } else {
-      res <- results(dds)
-    }
+  } else {
+    counts <- count_data %>%
+      dplyr::select(contains(type))
     
-    sig_res <- res %>%
-      as_tibble(rownames = "geneid") %>%
-      dplyr::filter(
-        abs(log2FoldChange) >= log2(fold_change_thresh) &
-          padj < pval_thresh & baseMean >= base_mean_thresh
-      )
+    detected.genes <-
+      rownames(counts)[rowMeans(counts) >= average_counts]
     
-    # annotate protein coding genes
-    pcg_sig_res <- sig_res %>%
-      filter(!str_detect(geneid, "NR|XR")) %>%
-      mutate(symbol = gsub("_.*", "", geneid)) %>%
-      left_join(CriGri_PICRH_1_0_annotation, by = "symbol") %>%
-      filter(`# feature` == "mRNA") %>%
-      mutate(name = gsub(",.*", "", name)) %>%
-      distinct(geneid, .keep_all = T) %>%
-      dplyr::select("symbol", "name", "GeneID", c(colnames(sig_res)))
-    
-    # gene names causes a problem with the regex in fuzzy match
-    # pcg_sig_re <- pcg_sig_res %>%
-    #   mutate(symbol = replace(symbol, name == "amine oxidase [flavin-containing] A", "Maoa"))
-    #
-    
-    pcg_without_loc_ids <- pcg_sig_res %>%
-      filter(!str_detect(symbol, "LOC"))
-    
-    pcg_with_loc_ids <- pcg_sig_res %>%
-      filter(str_detect(symbol, "LOC"))
-    
-    # match the names against mouse
-    loc_id_gene_symbol <- mouse_feature_table %>%
-      mutate(ncbi_symbol = symbol) %>%
-      fuzzy_inner_join(pcg_with_loc_ids, by = "name", match_fun = str_detect) %>%
-      distinct(symbol.y, .keep_all = T)
-    
-    pcg_with_loc_ids <- pcg_with_loc_ids %>%
-      mutate("symbol.y" = symbol) %>%
-      left_join(loc_id_gene_symbol, by = "symbol.y") %>%
-      mutate(ncbi_symbol = coalesce(ncbi_symbol, symbol.y)) %>%
-      mutate(
-        "geneid" = geneid.x,
-        "baseMean" = baseMean.x,
-        "log2FoldChange" = log2FoldChange.x,
-        "lfcSE" = lfcSE.x,
-        "stat" = stat.x,
-        "pvalue" = pvalue.x,
-        "padj" = padj.x,
-        "symbol" = ncbi_symbol
-      ) %>%
-      dplyr::select(symbol, name, GeneID, c(colnames(sig_res)))
-    
-    nc_sig_res <- sig_res %>%
-      filter(str_detect(geneid, "NR|XR")) %>%
-      mutate(symbol = "New",
-             name = "New",
-             GeneID = 0) %>%
-      dplyr::select("symbol",
-                    "name",
-                    "GeneID",
-                    c(colnames(sig_res)))
-    
-    sig_res <-
-      bind_rows(pcg_without_loc_ids, pcg_with_loc_ids, nc_sig_res) %>%
-      arrange(-log2FoldChange) %>%
-      dplyr::select(
-        c(
-          "geneid",
-          "GeneID",
-          "symbol",
-          "name",
-          "baseMean",
-          "log2FoldChange",
-          "lfcSE",
-          "stat",
-          "pvalue",
-          "padj"
-        )
-      ) %>%
-      dplyr::rename(Plastid_ID = "geneid")
-    
-    return(list(
-      deseq_object = dds,
-      deseq_res = res,
-      significant = sig_res
-    ))
-    
+    counts <- counts[detected.genes,]
   }
+
+  # initiate deseq object
+  if (type == "translation") {
+    dds <- DESeqDataSetFromMatrix(
+      countData = counts,
+      colData = sample_table,
+      design = ~ condition + assay + condition:assay
+    )
+    
+    # set relevel
+    dds$condition <- relevel(dds$condition, ref = "nts")
+    dds$assay <- relevel(dds$assay, ref = "rnaseq")
+
+  } else {
+    design <- sample_table %>%
+      filter(assay == type)
+    
+    dds <-
+      DESeqDataSetFromMatrix(
+        countData = counts,
+        colData = design,
+        design = ~ condition
+      )
+
+    # set relevel
+    dds$condition <- relevel(dds$condition, ref = "nts")
+  }
+
+  dds <- estimateSizeFactors(dds)
+  dds <- estimateDispersions(dds)
+  dds <- DESeq(dds)
+
+  # select contrast
+  if (type == "translation") {
+    res = results(dds, name = "conditionts.assayriboseq")
+  } else {
+    print(resultsNames(dds))
+    res <- results(dds, name= "condition_ts_vs_nts")
+    res = lfcShrink(dds,coef="condition_ts_vs_nts",res=res)
+  }
+
+  # significant genes
+  sig_res <- res %>%
+    as_tibble(rownames = "geneid") %>%
+    dplyr::filter(
+      abs(log2FoldChange) >= log2(fold_change_threshold) &
+        padj < pval_threshold & baseMean >= base_mean_threshold
+    )
+
+  # annotate protein coding genes if possible using cgr ncbi 
+  pcg_sig_res <- sig_res %>%
+    filter(!str_detect(geneid, "NR|XR")) %>%
+    mutate(symbol = gsub("_.*", "", geneid)) %>%
+    left_join(CriGri_PICRH_1_0_annotation, by = "symbol") %>%
+    filter(`# feature` == "mRNA") %>%
+    mutate(name = gsub(",.*", "", name)) %>%
+    distinct(geneid, .keep_all = T) %>%
+    dplyr::select("symbol", "name", "GeneID", c(colnames(sig_res)))
+
+  # determine genes successfully assigned to gene symbols
+  pcg_without_loc_ids <- pcg_sig_res %>%
+    filter(!str_detect(symbol, "LOC"))
+
+  # determine genes with no symbol annotation
+  pcg_with_loc_ids <- pcg_sig_res %>%
+    filter(str_detect(symbol, "LOC"))
+
+  # match the long gene name against mouse using fuzzy match
+  loc_id_gene_symbol <- mouse_feature_table %>%
+    mutate(ncbi_symbol = symbol) %>%
+    fuzzy_inner_join(pcg_with_loc_ids, by = "name", match_fun = str_detect) %>%
+    distinct(symbol.y, .keep_all = T)
+
+  pcg_with_loc_ids <- pcg_with_loc_ids %>%
+    mutate("symbol.y" = symbol) %>%
+    left_join(loc_id_gene_symbol, by = "symbol.y") %>%
+    mutate(ncbi_symbol = coalesce(ncbi_symbol, symbol.y)) %>%
+    mutate(
+      "geneid" = geneid.x,
+      "baseMean" = baseMean.x,
+      "log2FoldChange" = log2FoldChange.x,
+      "lfcSE" = lfcSE.x,
+      "stat" = stat.x,
+      "pvalue" = pvalue.x,
+      "padj" = padj.x,
+      "symbol" = ncbi_symbol
+    ) %>%
+    dplyr::select(symbol, name, GeneID, c(colnames(sig_res)))
+
+  # annotate new ORFs
+  nc_sig_res <- sig_res %>%
+    filter(str_detect(geneid, "NR|XR")) %>%
+    mutate(symbol = "New", name = "New",
+            GeneID = 0) %>%
+    dplyr::select("symbol", "name", "GeneID",
+                  c(colnames(sig_res)))
+
+  # complete the table of significant results
+  sig_res <-
+    bind_rows(pcg_without_loc_ids, pcg_with_loc_ids, nc_sig_res) %>%
+    arrange(-log2FoldChange) %>%
+    dplyr::select(c("geneid", "GeneID", "symbol", "name", "baseMean", "log2FoldChange",
+    "lfcSE","stat","pvalue","padj")
+    ) %>%
+    dplyr::rename(Plastid_ID = "geneid")
+
+  return(list(
+    deseq_object = dds,
+    deseq_res = res,
+    significant = sig_res
+  ))
+
+}
--- scripts/differential_translation/run_deseq2.R
+++ scripts/differential_translation/run_deseq2.R
@@ -13,70 +13,82 @@ package_list <- c(
 
 invisible(lapply(package_list, require, character.only = TRUE))
 
-source("scripts/orf_filtering_functions.R")
+source("./scripts/differential_translation/deseq_utility_functions.R")
 
 results_dir <- "differential_translation/deseq_output"
 if (!dir.exists(results_dir)) {
   dir.create(results_dir, recursive = TRUE)
 }
 
-# load the counts
-count_data <- read.delim("differential_translation/gene_cds_counts.txt",
-  sep = "\t", header = TRUE, row.names = "region")
-rownames(count_data)[which(rownames(count_data) == "LOC100760815_1")] <- "Maoa"
+############################################################
+# 1. import annotation files
+############################################################
 
-sample_table <- read.csv("data/experimental_design/sequencing_design.txt",
+# plastid
+reference_annotation_plastid_merged <- read_delim(
+  "differential_translation/plastid_reference/annotation_merged.txt",
+  "\t", 
+  escape_double = FALSE, 
+  col_names = FALSE, 
+  trim_ws = TRUE, 
+  skip = 27, 
+  show_col =F) %>% 
+  mutate(X3 = str_remove(X1, "gene_")) 
+
+# Chinese hamster  
+CriGri_PICRH_1_0_annotation <- read_delim(
+  "reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_feature_table.txt",
+  "\t", 
+  escape_double = FALSE, 
+  trim_ws = TRUE, 
+  show_col =F)
+
+# load the mouse annotation - used to match gene names to LOC ids
+mouse_feature_table <- read_delim(
+  "reference_genome/GCF_000001635.27_GRCm39_feature_table.txt",
+  "\t", 
+  escape_double = FALSE, 
+  trim_ws = TRUE, 
+  show_col =F)
+
+############################################################
+# 2. Determine differential expression and translation
+############################################################
+
+# load the counts
+count_data <- read.delim("differential_translation/combined_cds_counts.txt",
+  sep = "\t", header = TRUE, row.names = "region")
+
+# gene name causes a problem with the regex in fuzzy match, so we change it here manually
+rownames(count_data)[which(rownames(count_data) == "LOC100760815")] <- "Maoa"
+
+# experimental design
+sample_table <- read.csv("data/differential_translation/experimental_design.txt",
   header = TRUE,  row.names = "sample_name")
 
-# import the annotation
-reference_annotation_plastid_merged <- read_delim("differential_translation/plastid_reference/annotation_merged.txt",
-  "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE, skip = 27
-) %>% mutate(X3 = str_remove(X1, "gene_")) %>%
-  mutate(`product_accession` = str_remove(X3, "_1"))
+sample_table$condition <- as.factor(sample_table$condition)
+sample_table$assay <- as.factor(sample_table$assay)
 
-# import the ncbi annotation
-CriGri_PICRH_1_0_annotation <- read_delim( "reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_feature_table.txt",
-  "\t", escape_double = FALSE, trim_ws = TRUE)
-
-# load the mouse annotation - used to match gene names to complete LOC to symbol conversion
-mouse_feature_table <- read_delim("reference_genome/GCF_000001635.27_GRCm39_feature_table.txt",
-  "\t",   escape_double = FALSE, trim_ws = TRUE)
-
-# Differential expression and translation
 # set the differential expression/translation criteria
 fold_change_threshold <- 1.5
 pval_threshold <- 0.05
 base_mean_threshold <- 0
-average_counts <- 20
+average_counts <- 10
 
-# 2. RNA-seq only differential expression
+# 1. differential gene expression
+differential_expression <-  run_deseq(count_data, sample_table,
+fold_change_threshold, pval_threshold, base_mean_threshold, average_counts,
+"rnaseq", CriGri_PICRH_1_0_annotation,mouse_feature_table)
 
-differential_expression <-  run_deseq(count_data,
-                                       sample_table,
-                                       fold_change_threshold, 
-                                       pval_threshold, 
-                                       base_mean_threshold, 
-                                       average_counts,
-                                       "rnaseq",
-                                       CriGri_PICRH_1_0_annotation,
-                                       mouse_feature_table)
+# 2. differential RPF 
+differential_rpf <-  run_deseq(count_data, sample_table, fold_change_threshold, 
+pval_threshold, base_mean_threshold, average_counts,"riboseq", CriGri_PICRH_1_0_annotation, mouse_feature_table)
 
-differential_rpf <-  run_deseq(count_data,
-                                      sample_table,
-                                      fold_change_threshold, 
-                                      pval_threshold, 
-                                      base_mean_threshold, 
-                                      average_counts,
-                                      "riboseq",
-                                      CriGri_PICRH_1_0_annotation,
-                                      mouse_feature_table)
+# 3. differential translation efficency
+differential_translation <-  run_deseq(count_data,sample_table, fold_change_threshold, pval_threshold, base_mean_threshold, 
+average_counts,"translation", CriGri_PICRH_1_0_annotation,mouse_feature_table)
 
-differential_translation <-  run_deseq(count_data,sample_table, fold_change_threshold, 
-          pval_threshold, 
-          base_mean_threshold, 
-          average_counts,
-          "translation",
-          CriGri_PICRH_1_0_annotation,
-          mouse_feature_table)
+
+
 
 # save(differential_expression,differential_rpf,differential_translation, file = paste(results_dir, "/deseq_output.RData", sep = ""))
