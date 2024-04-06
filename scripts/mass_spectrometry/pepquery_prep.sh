643435eb90dd3343915429c87efaac144c9f929c
colin986 <colin.clarke@nibrt.ie>
2024-04-06 08:52:12 +0000

    update

--- scripts/mass_spectrometry/pepquery_prep.sh
+++ scripts/mass_spectrometry/pepquery_prep.sh
@@ -4,6 +4,7 @@
 ####    1. semi tryptic peptides for microproteins
 ####    2. make a reference proteome for pepquery
 ####    3. convert the mass calibrated mzML files to MGF for HCP and lysate
+####    4. make pepquery index
 #### Written by: NIBRT Clarke Lab. - colin.clarke@nibrt.ie
 
 mkdir $PWD/proteomics/pepquery && pepquery_dir=$_
@@ -24,14 +25,25 @@ conda deactivate
 
 # peptide to protein mapping file
 grep ',2,' $digest_dir/prosit_input.csv | cut -d, -f1  > \
-$digest_dir/microprotein_peptides.txt
+$digest_dir/microprotein_all_peptides.txt
 
 mv $digest_dir/prosit_input_with_proteins.csv \
 $digest_dir/microprotein_peptide_mapping.csv
 
-#### reference proteome
+# we need to do two separate pepquery runs for each data type
+# one is for all peptides with on ox on M as var mod
+# the second is a reduced search of the peptides with the n-term of the protein
+# here, we add n-term acetylation and ox on M as var mods
+
+awk '/^>/ {if (seq) {print substr(seq, 1, 7)}; seq=""; next} {seq = seq $0} END {if (seq) print substr(seq, 1, 7)}' \
+$digest_dir/microproteins.fasta | awk '{print "^" substr($0, 1, 7)}' | \
+grep -f - $digest_dir/microprotein_all_peptides.txt > \
+$digest_dir/microprotein_nterm_peptides.txt
+
+# known proteins
 mkdir $pepquery_dir/known_proteome && ref_proteome=$_
 
+# uniprot and cRAP
 cp reference_proteome/*.fasta $ref_proteome
 
 # all mabs
@@ -42,22 +54,35 @@ cp data/protein_sequences/standards/* $ref_proteome
 
 find $ref_proteome/*.fasta -type f -exec cat {} \; > $ref_proteome/known_proteins.fasta
 
-#### file conversion
-# make MGF format files from mass calibrated files
+# Construct pepquery index 
+# 1. make MGF format files from mzML mass calibrated files
+# 2. make an index for the drug product and lysate ms data
 
-type=("lysate")
+data_types=("drug_product" "lysate")
 
-for type in "${type[@]}"; do
+pepquery_jar=$CONDA_PREFIX/share/pepquery-2.0.2-0/pepquery-2.0.2.jar
 
-    mkdir -p $pepquery_dir/mgf_files/$type && mgf_dir=$_
+source activate microprotein_process_env
 
-    find proteomics/metamorpheus/$type -type f -name "*calib*.mzML" > \
-    $pepquery_dir/mgf/"$type"_calibrated_file_list.txt
+for data_type in "${data_types[@]}"; do
 
-    # source activate microprotein_process_env
+    mkdir -p $pepquery_dir/mgf_files/$data_type && mgf_dir=$_
+
+    # list mass calibrated mzML files
+    find proteomics/metamorpheus/$data_type -type f -name "*calib*.mzML" > \
+    $pepquery_dir/mgf_files/"$data_type"_calibrated_file_list.txt
+    
+    # convert mzML to MGF
     msconvert --mgf \
-    -f $pepquery_dir/mgf/"$type"_calibrated_file_list.txt \
+    -f $pepquery_dir/mgf_files/"$data_type"_calibrated_file_list.txt \
     -o $mgf_dir
-    # conda deactivate
+
+    # build the pepquery index
+    mkdir -p $pepquery_dir/pepquery_index/$data_type && index_dir=$_
+
+    java -Xmx275G -jar $pepquery_jar index \
+    -c 70 -f mgf -i $mgf_dir -o $index_dir
 
 done
+
+conda deactivate
--- scripts/mass_spectrometry/run_pepquery.sh
+++ scripts/mass_spectrometry/run_pepquery.sh
@@ -1,3 +1,46 @@
 #!/bin/bash
 
+#### Description: preparation for the pepuery analysis of microproteins 
+####    1. semi tryptic peptides for microproteins
+####    2. make a reference proteome for pepquery
+####    3. convert the mass calibrated mzML files to MGF for HCP and lysate
+####    4. make pepquery index
+#### Written by: NIBRT Clarke Lab. - colin.clarke@nibrt.ie
 
+# specific the path the jar to allow setting -Xmx 
+pepquery_jar=$CONDA_PREFIX/share/pepquery-2.0.2-0/pepquery-2.0.2.jar
+
+pepquery_dir=proteomics/pepquery
+mkdir $pepquery_dir/pepquery_results && $pepquery_result
+
+data_types=("drug_product" "lysate")
+search_types=("all" "nterm")
+
+for data_type in "${data_types[@]}"; do
+    
+    for search_type in "${search_types[@]}"; do
+
+        mkdir $pepquery_result/$data_type/$search_type & pepquery_out=$_
+
+        if [ "$search_type" = "all" ]; then
+            modification="2"
+        else
+            modification="2,5"
+        fi
+
+        java -Xmx250G -jar $pepquery_jar \
+        -i $pepquery_dir/peptide_digestion/microprotein_"$search_type"_peptides.txt \
+        -t peptide -s 1 -c 2 \
+        -varMod $modification \
+        -tol 10 \
+        -itol 0.05 \
+        -minLength 7 \
+        -maxLength 45 \
+        -db $pepquery_dir/known_proteome/known_proteins.fasta \
+        -ms $pepquery_dir/pepquery_index/$data_type \
+        -cpu 70 \
+        -o $pepquery_out \
+        1> $pepquery_dir/pepquery_search_"$data_type"_"$search_type"_log.txt
+
+    done
+done
