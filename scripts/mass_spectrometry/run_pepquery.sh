e05e21726edf83c922ab73ac78cc5cecfd6354e8
colin986 <colin.clarke@nibrt.ie>
2024-04-06 15:39:57 +0000

    updated filepath

--- scripts/mass_spectrometry/run_pepquery.sh
+++ scripts/mass_spectrometry/run_pepquery.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 #### Description: preparation for the pepuery analysis of microproteins 
-####    1. semi tryptic peptides for microproteins
-####    2. make a reference proteome for pepquery
-####    3. convert the mass calibrated mzML files to MGF for HCP and lysate
-####    4. make pepquery index
+####    1. use pepquery to search the drug product and lysate data
+####    2. search all microprotein peptides and the n-term with acetylation
 #### Written by: NIBRT Clarke Lab. - colin.clarke@nibrt.ie
+
+pepquery_dir=proteomics/pepquery
+mkdir $pepquery_dir/results && pepquery_result=$_
+
+data_types=("lysate")
+search_types=("nterm")
+
+source activate microprotein_process_env
 
 # specific the path the jar to allow setting -Xmx 
 pepquery_jar=$CONDA_PREFIX/share/pepquery-2.0.2-0/pepquery-2.0.2.jar
-
-pepquery_dir=proteomics/pepquery
-mkdir $pepquery_dir/pepquery_results && $pepquery_result
-
-data_types=("drug_product" "lysate")
-search_types=("all" "nterm")
 
 for data_type in "${data_types[@]}"; do
     
     for search_type in "${search_types[@]}"; do
 
-        mkdir $pepquery_result/$data_type/$search_type & pepquery_out=$_
+        mkdir -p $pepquery_result/$data_type/$search_type && pepquery_out=$_
 
+        # set the variable modifications based on search type
         if [ "$search_type" = "all" ]; then
             modification="2"
         else
@@ -37,10 +38,11 @@ for data_type in "${data_types[@]}"; do
         -minLength 7 \
         -maxLength 45 \
         -db $pepquery_dir/known_proteome/known_proteins.fasta \
-        -ms $pepquery_dir/pepquery_index/$data_type \
-        -cpu 70 \
-        -o $pepquery_out \
+        -ms $pepquery_dir/index/$data_type \
+        -cpu 70 -o $pepquery_out \
         1> $pepquery_dir/pepquery_search_"$data_type"_"$search_type"_log.txt
 
     done
 done
+
+conda deactivate
