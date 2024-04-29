70a9256adf0dffbc48eaf0651ff0c4647ae776e4
colin986 <colin.clarke@nibrt.ie>
2024-04-29 09:20:45 +0000

    annotate the classified events output

--- scripts/differential_translation/run_deseq2.R
+++ scripts/differential_translation/run_deseq2.R
@@ -115,9 +115,6 @@ res_te <- te_deseq$deseq_res
 res_rna <- rna_deseq$deseq_res[rownames(res_te),]
 res_ribo <- rpf_deseq$deseq_res[rownames(res_te),]
 
-
-
-
 fold_change_threshold <- 0
 transcriptional_select=which(res_te$padj >= 0.05 & 
 res_ribo$padj < 0.05 & abs(res_ribo$log2FoldChange) >= log2(fold_change_threshold) & 
@@ -157,47 +154,68 @@ mutate(class=case_when(
 
 print(table(classified_events$class))
 
+# maoa is causing a problem    
+moao_annoation <- classified_events %>% 
+filter(gene_id=="Maoa") %>%
+mutate(symbol = "Maoa",
+GeneID = 100760815,
+name = "amine oxidase [flavin-containing] A")
+
+
+pcg_classified <- classified_events %>%
+  filter(!str_detect(gene_id, "NR|XR"))%>% 
+  filter(class != "undetermined") %>%
+  mutate(symbol = gsub("_.*", "", gene_id)) %>%
+  left_join(CriGri_PICRH_1_0_annotation, by = "symbol", relationship = "many-to-many") %>%
+  filter(`# feature` == "mRNA") %>%
+  mutate(name = gsub(",.*", "", name)) %>%
+  distinct(gene_id, .keep_all = T) %>%
+  dplyr::rename(class=class.y) %>%
+  dplyr::select("symbol", "name", "GeneID", c(colnames(classified_events)))
+
+# determine genes successfully assigned to gene symbols
+pcg_without_loc_ids <- pcg_classified %>%
+  filter(!str_detect(symbol, "LOC"))
+
+# determine genes with no symbol annotation
+pcg_with_loc_ids <- pcg_classified %>%
+  filter(str_detect(symbol, "LOC"))
+
+# match the long gene name against mouse using fuzzy match
+loc_id_gene_symbol <- mouse_feature_table %>%
+  mutate(ncbi_symbol = symbol) %>%
+  fuzzy_inner_join(pcg_with_loc_ids, by = "name", match_fun = str_detect) %>%
+  distinct(symbol.y, .keep_all = T)
+
+pcg_with_loc_ids <- pcg_with_loc_ids %>%
+mutate("symbol.y" = symbol) %>%
+left_join(loc_id_gene_symbol, by = "symbol.y") %>%
+mutate(ncbi_symbol = coalesce(ncbi_symbol, symbol.y)) %>%
+mutate(
+  "gene_id" = gene_id.x,
+  "rna_fc" = rna_fc.x,
+  "rna_padj" = rna_padj.x,
+  "rpf_fc" = rpf_fc.x, 
+  "rpf_padj" = rpf_padj.x,
+  "te_fc" = te_fc.x,
+  "te_padj" = te_padj.x,
+  "symbol" = ncbi_symbol
+) %>%
+dplyr::select(gene_id, symbol, name, GeneID, rna_fc, rna_padj, rpf_fc, rpf_padj, te_fc, te_padj, class)
+
+# annotate new ORFs
+nc_sig_res <- classified_events %>%
+filter(class != "undetermined") %>%
+filter(str_detect(gene_id, "NR|XR")) %>%
+mutate(symbol = "New", name = "New",
+        GeneID = 0) %>%
+dplyr::select(gene_id, symbol, name, GeneID, rna_fc, rna_padj, rpf_fc, rpf_padj, te_fc, te_padj, class )
+
+classified_event_annotation <-
+bind_rows(moao_annoation, pcg_without_loc_ids, pcg_with_loc_ids, nc_sig_res) %>%
+dplyr::select(c("gene_id", "GeneID", "symbol", "name")
+) 
+
+
 saveRDS(classified_events, file = paste(results_dir, "/classified_events.rds", sep = ""))
-
-
-
-# colorblind_palette <- c("#377eb8", "#e41a1c", "#4daf4a", "#984ea3", "#999999")
-# max_fc =max(abs(c(joined$rna_fc, joined$rpf_fc)))
-
-# joined %>%
-# arrange(desc(class)) %>%
-# ggplot(aes(x=rna_fc, y=rpf_fc, color=class)) +
-# geom_abline(intercept = 0, slope = 1, color = "grey") +  # Add diagonal line through origin
-# geom_vline(xintercept=c(-log2(1.5),0, log2(1.5)), color = "grey") +
-# geom_hline(yintercept=c(-log2(1.5),0, log2(1.5)), color = "grey") +
-# geom_point(size=0.6, alpha=0.8) +
-# scale_color_manual(values=colorblind_palette) +
-# geom_text_repel(data = subset(joined, str_detect(gene_id, "XR") & class != "undetermined" & class != "forwarded"), aes(label = gene_id), box.padding = 0.5) +
-# labs(x = expression("Log"[2] ~ "mRNA FC (TS/NTS)"), 
-# y = expression("Log"[2] ~ "RPF FC (TS/NTS)"), color = "", alpha = "") +
-# xlim(-max_fc, max_fc) +
-# ylim(-max_fc, max_fc) +
-# theme_bw() +
-# guides(color = guide_legend(override.aes = list(size = 3)))
-
-# b <- joined %>%
-# filter(class != "undetermined") %>%
-# filter(str_detect(gene_id, "XR|NR"))
-
-# ggscatterhist(
-#   joined, x = "rna_fc", y = "rpf_fc",
-#   color = "class", size = 0.1, alpha = 0.6,
-#   palette = c("#00AFBB", "#E7B800", "#FC4E07", "green", "blue"),
-#   margin.params = list(fill = "class", color = "black", size = 0.2)
-#   )
-
-# # save(differential_expression,rpf_deseq,te_deseq, file = paste(results_dir, "/deseq_output.RData", sep = ""))
-
-
-# ggplot(data.frame(res_rna), aes(x = log2FoldChange, y = -log10(pvalue))) +
-#   geom_point(size=0.1) +
-#   geom_vline(xintercept = c(-log2(1.5), log2(1.5)), linetype = "dashed", color = "blue") +
-#   geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "blue") +
-#   scale_color_manual(values = c("black" = "black", "red" = "red")) +
-#   labs(x = "log2(Fold Change)", y = "-log10(p-value)", title = "Volcano Plot") +
-#   theme_minimal()
+saveRDS(classified_event_annotation, file = paste(results_dir, "/classified_event_annotation.rds", sep = ""))
